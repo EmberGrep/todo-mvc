b58c348d2e74a8084e9509a70a1f5898d6494e57
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-20 23:09:49 -0600

    Todos can be deleted from component

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     deleteTodo(todo) {
-
+      this.sendAction('ondestroy', todo);
     },
 
     toggleDone(todo) {
