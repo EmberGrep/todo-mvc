103f99f1094ade8ab7fcc923873cb2840ed863dc
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-21 00:25:47 -0600

    Show number of todos

---
+++ tests/unit/helpers/arr-length-test.js
@@ -0,0 +1,10 @@
+import { arrLength } from '../../../helpers/arr-length';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | arr length');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = arrLength([42]);
+  assert.ok(result);
+});
