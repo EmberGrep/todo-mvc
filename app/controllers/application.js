7639db868206ecefddd9a7dd03446690597ed404
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-20 18:52:14 -0600

    Submit todo to server

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,16 @@ export default Ember.Controller.extend({
   actions: {
     createTodo(title, ev) {
       ev.preventDefault();
-      console.log(title);
+      window.fetch('http://todo-mvc-api.herokuapp.com/api/todos', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({todo: {title}})
+      })
+        .then((response) => response.json())
+        .then((data) => console.log('request succeeded with JSON response', data));
     }
   }
 });
