7201639e856304d31b1054de6a6516ec057cdcef
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-28 10:45:24 -0600

    Active only test suite

--- tests/unit/helpers/active-only-test.js
+++ tests/unit/helpers/active-only-test.js
@@ -25,3 +25,40 @@ test('it returns a good default for non-arrays', function(assert) {
   let resultStr = activeOnly([str]);
   assert.deepEqual(resultStr, []);
 });
+
+test('it returns an array of active todos', function(assert) {
+  let todos = [
+    {title: 'Cereal', isComplete: false},
+    {title: 'Milk', isComplete: false},
+  ];
+
+  let resultObj = activeOnly([todos]);
+  assert.deepEqual(resultObj, todos);
+});
+
+test('it returns no tasks if all are done', function(assert) {
+  let todos = [
+    {title: 'Cereal', isComplete: true},
+    {title: 'Milk', isComplete: true},
+  ];
+
+  let resultObj = activeOnly([todos]);
+  assert.deepEqual(resultObj, []);
+});
+
+test('it returns only incomplete tasks from a mixed set', function(assert) {
+  let todos = [
+    {title: 'Cereal', isComplete: true},
+    {title: 'Milk', isComplete: false},
+  ];
+
+  let resultObj = activeOnly([todos]);
+  assert.deepEqual(resultObj, [{title: 'Milk', isComplete: false}]);
+});
+
+test('it returns empty when there are no todos', function(assert) {
+  let todos = [];
+
+  let resultObj = activeOnly([todos]);
+  assert.deepEqual(resultObj, []);
+});
