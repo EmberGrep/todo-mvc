19ef9f90df5d3c427c410ed6fe8e459101c2db1e
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-28 10:36:47 -0600

    Default tests for `complete-only`

--- app/helpers/active-only.js
+++ app/helpers/active-only.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export function activeOnly([todos]/*, hash*/) {
+  if (typeof todos !== 'object' || typeof todos.filter !== 'function') {
+    return [];
+  }
+
   return todos.filter((todo) => {
     return !todo.isComplete;
   });
--- tests/unit/helpers/active-only-test.js
+++ tests/unit/helpers/active-only-test.js
@@ -4,7 +4,24 @@ import { module, test } from 'qunit';
 module('Unit | Helper | active only');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = activeOnly([42]);
-  assert.ok(result);
+test('it returns a good default for non-arrays', function(assert) {
+  let obj = {foo: 'bar'};
+
+  let resultObj = activeOnly([obj]);
+  assert.deepEqual(resultObj, []);
+
+  let n = {foo: 'bar'};
+
+  let resultNull = activeOnly([n]);
+  assert.deepEqual(resultNull, []);
+
+  let u = undefined;
+
+  let resultUndefined = activeOnly([u]);
+  assert.deepEqual(resultUndefined, []);
+
+  let str = `I'm a teapot`;
+
+  let resultStr = activeOnly([str]);
+  assert.deepEqual(resultStr, []);
 });
